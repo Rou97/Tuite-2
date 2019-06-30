33db68795b0998bcc747f835f3b9ed567e393275
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-06-30 19:06:46 +0200

    practising middlewares with firebase

--- functions/index.js
+++ functions/index.js
@@ -27,6 +27,35 @@ firebase.initializeApp(config);
 
 const db = admin.firestore();
 
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return res.status(403).json({ error: 'Unauthorized' });
+    }
+  
+    admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+        req.user = decodedToken;
+        console.log(decodedToken);
+        return db
+            .collection('users')
+            .where('userId', '==', req.user.uid)
+            .limit(1)
+            .get();
+        })
+        .then((data) => {
+        req.user.handle = data.docs[0].data().handle;
+        return next();
+        })
+        .catch((err) => {
+        console.error('Error while verifying token ', err);
+        return res.status(403).json(err);
+        });
+    };
+
 const isEmail = (email) => {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(email.match(regEx)) return true;
@@ -100,8 +129,8 @@ app.post('/signup', (req, res) => {
 
 app.post('/login', (req, res) => {
     const user = {
-      email: req.body.email,
-      password: req.body.password
+        email: req.body.email,
+        password: req.body.password
     };
   
     let errors = {};
@@ -112,19 +141,19 @@ app.post('/login', (req, res) => {
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
   
     firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-      .then((data) => {
+        .then((data) => {
         return data.user.getIdToken();
-      })
-      .then((token) => {
+        })
+        .then((token) => {
         return res.json({ token });
-      })
-      .catch((err) => {
+        })
+        .catch((err) => {
         console.error(err);
         if (err.code === 'auth/wrong-password') {
-          return res.status(403).json({ general: 'Wrong credentials, please try again' });
+            return res.status(403).json({ general: 'Wrong credentials, please try again' });
         } else return res.status(500).json({ error: err.code });
-      });
-  });
+        });
+    });
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
 
