48cfbf5b88f715ffcec3a31cc08b83fa3676bcf1
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-07-15 16:04:07 +0200

    fix createNotoficationOnComment

--- functions/index.js
+++ functions/index.js
@@ -90,7 +90,7 @@ exports.createNotificationOnComment = functions
 		return db.doc(`/screams/${snapshot.data().screamId}`)
 			.get()
 			.then((doc) => {
-				if (doc.exists) {
+				if (doc.exists && doc.data().userHandle !== snapshot.data().userHandle) {
 					return db.doc(`/notifications/${snapshot.id}`).set({
 					createdAt: new Date().toISOString(),
 					recipient: doc.data().userHandle,
